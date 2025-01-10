7d61951adcd72a022957fa215dfcbbef45d71517
LS351713 <luiss23131@gmail.com>
2025-01-09 19:10:57 -0500

    Initial commit - Rock-Paper-Scissors Game

--- script.js
+++ script.js
@@ -48,11 +48,11 @@ const playGame = (playerChoice) => {
 let computerScore = 0;
 const updateScores = (result) => {
  if (result === "You win!") {
-playerScore++;
+playerScore+ 1;
  //update playerScore
  //update the textContent of tag element id# "player-score"
  } else if (result === "Computer wins!") {
-    computerScore++;
+    computerScore+ 1;
  //update computerScore
  //update the textContent of tag element id# "computer-score"
  }
